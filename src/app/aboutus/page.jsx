e99faef6d75c0a04a38eeea7acfc41cde0d24176
huntermahmud <mahmudthedatahunter@gmail.com>
2024-09-21 17:44:32 +0600

    add: aboutus page added

---
+++ src/app/aboutus/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactUs from './../../components/ContactUs';
+
+const AboutUsPage = () => {
+    return (
+        <div>
+            
+           <ContactUs/>
+        </div>
+    );
+};
+
+export default AboutUsPage;
