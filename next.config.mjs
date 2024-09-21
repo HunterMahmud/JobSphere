61a39f68c7d685621eb3b1176fef3e1b87bd3787
huntermahmud <mahmudthedatahunter@gmail.com>
2024-09-21 17:54:58 +0600

    update: next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        hostname: "**", // Correct hostname for imagebb
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
