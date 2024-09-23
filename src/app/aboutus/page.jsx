a5eae33b4fa490f0405457e59f221f59fed79cb6
huntermahmud <mahmudthedatahunter@gmail.com>
2024-09-23 19:13:25 +0600

    folder structured updated

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import ContactUs from './../../components/ContactUs';
+import ContactUs from '../../components/shared/ContactUs';
 
 const AboutUsPage = () => {
     return (
         <div>
-            
            <ContactUs/>
         </div>
     );
