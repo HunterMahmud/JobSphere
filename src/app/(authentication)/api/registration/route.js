52c73d1cc7905d317cdec7e9547daa85caa3a05d
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-09-24 15:44:50 +0600

    folder structure modified

--- src/app/(authentication)/api/registration/route.js
+++ src/app/(authentication)/api/registration/route.js
@@ -9,34 +9,76 @@ export const POST = async (request) => {
     const db = await connectDB();
     const userCollection = db.collection("users");
 
-    // Check if the user exists
     const exist = await userCollection.findOne({ email: newUser.email });
-    console.log("Existing User:", exist); // Debugging info
+    console.log("Existing User:", exist); 
 
     if (exist) {
-      // Return 409 Conflict when user already exists
       return NextResponse.json({ message: "User Exists" }, { status: 409 });
     }
 
-    // Hash the password
     const hashedPassword = bcrypt.hashSync(newUser.password, 14);
 
-    // Insert the new user with the hashed password
     const res = await userCollection.insertOne({
       ...newUser,
       password: hashedPassword,
     });
 
-    // User creation success
     return NextResponse.json({ message: "User Created" }, { status: 201 });
   } catch (error) {
-    // Log the error for debugging purposes
     console.error("Error while creating user:", error);
 
-    // Return error response with status 500
     return NextResponse.json(
       { message: "Something Went Wrong", error: error.message },
       { status: 500 }
     );
   }
 };
+
+
+
+// import { connectDB } from "@/lib/connectDB";
+// import bcrypt from "bcrypt";
+// import { NextResponse } from "next/server";
+
+// export const POST = async (request) => {
+//   const newUser = await request.json();
+  
+//   try {
+//     const db = await connectDB();
+    
+//     let collectionName;
+//     if (newUser.role === "employee") {
+//       collectionName = "employees"; 
+//     } else if (newUser.role === "admin") {
+//       collectionName = "admins"; 
+//     } else {
+//       collectionName = "users"
+//     }
+
+//     const userCollection = db.collection(collectionName);
+
+//     const exist = await userCollection.findOne({ email: newUser.email });
+//     console.log("Existing User:", exist); 
+
+//     if (exist) {
+//       return NextResponse.json({ message: "User Exists" }, { status: 409 });
+//     }
+
+//     const hashedPassword = bcrypt.hashSync(newUser.password, 14);
+
+//     // Insert new user
+//     const res = await userCollection.insertOne({
+//       ...newUser,
+//       password: hashedPassword,
+//     });
+
+//     return NextResponse.json({ message: "User Created" }, { status: 201 });
+//   } catch (error) {
+//     console.error("Error while creating user:", error);
+
+//     return NextResponse.json(
+//       { message: "Something Went Wrong", error: error.message },
+//       { status: 500 }
+//     );
+//   }
+// };
