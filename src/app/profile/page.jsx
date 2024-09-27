f73db8253453410f1e121424378c04cbad3b8127
rafizul896 <rafizulislam896@gmail.com>
2024-09-27 06:16:48 +0600

    add: profile page profile overview

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,11 +5,14 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { useForm } from "react-hook-form";
+import { FaRegEdit } from 'react-icons/fa';
+import { IoCloseSharp } from 'react-icons/io5';
 
 const profile = {
     "profileOverview": {
         "fullName": "John Doe",
-        "profilePicture": "profile-picture.jpg",
+        "profilePicture": "https://i.ibb.co.com/Swccb1r/Customer-Satisfaction.png",
         "professionalTitle": "Frontend Developer",
         "location": "Dhaka, Bangladesh",
         "resumeLink": "/resume.pdf"
@@ -135,17 +138,35 @@ function a11yProps(index) {
 }
 
 export default function VerticalTabs() {
+    const [edit, setEdit] = React.useState(false)
     const [value, setValue] = React.useState(0);
     const width = false;
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
-
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm();
+    const handleRegister = async data => {
+        const {
+            name,
+            email,
+            cityName,
+            education,
+            mobileNumber,
+            image,
+            password,
+            acceptTerms,
+        } = data;
+    }
     return (
         <div className='custom-container border-l-2'>
             <Box
                 sx={{ flexGrow: 1, bgcolor: 'background.paper', }}
-                className="flex flex-col md:flex-row"
+                className="flex flex-col md:flex-row relative my-6"
             >
                 <Tabs
                     orientation={`${width ? 'horizontal' : 'vertical'}`}
@@ -154,6 +175,7 @@ export default function VerticalTabs() {
                     onChange={handleChange}
                     aria-label="Vertical tabs example"
                     sx={{ borderRight: 1, borderColor: 'divider' }}
+                    className='min-w-[170px]'
                 >
                     <Tab label="Profile Overview" {...a11yProps(0)} className='capitalize' />
                     <Tab label="Personal Information" {...a11yProps(1)} className='capitalize' />
@@ -163,6 +185,103 @@ export default function VerticalTabs() {
                     <Tab label="Education" {...a11yProps(5)} className='capitalize' />
                     <Tab label="Certifications" {...a11yProps(6)} className='capitalize' />
                 </Tabs>
+                <div onClick={() => setEdit(!edit)} className='cursor-pointer absolute right-5 top- text-2xl font-semibold'>
+                    {edit ? <><IoCloseSharp /></> : <><FaRegEdit /></>}
+                </div>
+                <form onSubmit={handleSubmit(handleRegister)} className="space-y-6">
+
+                    <TabPanel value={value} index={0}>
+                        {/* Profile Overview */}
+                        {!edit ? <>
+                            <div className="max-w-3xl bg-white shadow-lg border rounded-lg p-8 mb-6">
+                                <div className="flex  items-center space-x-4">
+                                    <img
+                                        src={profile?.profileOverview?.profilePicture}
+                                        alt="Profile"
+                                        className="w-24 h-24 rounded-full"
+                                    />
+                                    <div>
+                                        <h2 className="text-2xl font-bold">{profile?.profileOverview?.fullName}</h2>
+                                        <p className="text-gray-500">{profile?.profileOverview?.professionalTitle}</p>
+                                        <p className="text-gray-400">{profile?.profileOverview?.location}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </> : <>
+                            <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-5">
+                                {/* Name */}
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-600 ">
+                                        Full Name
+                                    </label>
+                                    <input
+                                        {...register("name")}
+                                        defaultValue={profile.profileOverview.fullName}
+                                        type="text"
+                                        placeholder="Enter Your Name"
+                                        className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                                    />
+                                </div>
+                                {/* Professional Title */}
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-600 ">
+                                        Professional Title
+                                    </label>
+                                    <input
+                                        {...register("professionalTitle")}
+                                        defaultValue={profile?.profileOverview?.professionalTitle}
+                                        type="text"
+                                        placeholder="Enter Your Professional Title"
+                                        className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                                    />
+                                </div>
+                                {/* Location */}
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-600 ">
+                                        Location
+                                    </label>
+                                    <input
+                                        {...register("locatiom")}
+                                        defaultValue={profile.profileOverview.location}
+                                        type="text"
+                                        placeholder="Enter Your Name"
+                                        className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                                    />
+                                </div>
+                                {/* Name */}
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-600 ">
+                                        Full Name
+                                    </label>
+                                    <input
+                                        {...register("name")}
+                                        defaultValue={profile.profileOverview.fullName}
+                                        type="text"
+                                        placeholder="Enter Your Name"
+                                        className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                                    />
+                                </div>
+
+                            </div>
+                        </>}
+                    </TabPanel>
+
+                    {/* Submit button */}
+                    {
+                        edit && <>
+                            <div className='absolute right-3 bottom-0'>
+                                <div className='flex justify-end items-end'>
+                                    <button
+                                        type="submit"
+                                        className="bg-primary hover:bg-hoverColor px-5 rounded-md py-3 text-white"
+                                    >
+                                        Register
+                                    </button>
+                                </div>
+                            </div>
+                        </>
+                    }
+                </form>
             </Box>
         </div>
     );
