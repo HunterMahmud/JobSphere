992f132955a4dd36d874ce32e5a39407b25f2b08
tajtanvvir1 <tanvirah1223@gmail.com>
2024-10-09 16:57:33 +0600

    Home- Reveiws added

--- src/components/Homepage/Reviews.jsx
+++ src/components/Homepage/Reviews.jsx
@@ -34,6 +34,13 @@ const Reviews = () => {
             "review": "I found the user experience on JobSphere exceptional. As a designer myself, I appreciate how visually appealing and easy to navigate the platform is. I highly recommend it to anyone looking for new job opportunities."
           },
           {
+            "name": "John D.",
+            "role": "Marketing Manager",
+            "image": "https://imgbb.host/images/2U9n9.png",
+            "rating": 5,
+            "review": "I’ve tried a lot of job search platforms, but JobSphere stands out. The tailored recommendations were spot on, and I appreciated the variety of job listings from top companies. The blog section is also a great resource for upgrading my skills."
+          },
+          {
             "name": "Olivia M.",
             "role": "Product Manager",
             "image": "https://imgbb.host/images/2U9n9.png",
@@ -45,10 +52,10 @@ const Reviews = () => {
     const {name, role, rating, review} = userReviews;
     
     return (
-        <div className='mx-8'>
-           <h1 className='text-3xl font-bold text-center mb-8 underline'>Users Reviews</h1>
+        <div className='mx-4 my-10 md:m-16'>
+           <h1 className='text-3xl font-bold text-center mb-8 md:mt-20 underline'>Users Reviews</h1>
            {/* Job Cards */}
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-around md:gap-10">
+           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center md:gap-10 mx-auto items-center">
                 {Array.isArray(userReviews) && userReviews.length > 0 ? (
                     userReviews.map((reviews, index) => <ReviewsCard key={index} reviews={reviews} />)
                 ) : (
--- src/components/Homepage/ReviewsCard.jsx
+++ src/components/Homepage/ReviewsCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FaStar } from "react-icons/fa";
 
-const ReviewsCard = ({reviews}) => {
+const ReviewsCard = ({ reviews }) => {
+    const { name, role, rating, review } = reviews;
     return (
-        <div>
-            <h1>Card</h1>
+        <div className='bg-slate-100 transition hover:shadow-xl dark:bg-sky-50 border-sky-600 max-w-[400px] mx-auto min-h-[330px] dark:shadow-gray-800/25 rounded-lg border-2 py-4 px-6 my-4'>
+            <div className='text-center space-y-2'>
+                <div className='border-b-2 p-2 border-sky-300 rounded-lg'>
+                    <h1 className='text-xl font-bold underline-offset-2 underline'>Name: {name}</h1>
+                    <h1 className='text-lg font-semibold'>Position: {role}</h1>
+                </div>
+                <h1 className='font-semibold flex justify-center items-center gap-2'>Rating: {rating} <FaStar  className='text-lg text-sky-600 ' /></h1>
+                
+                <h1 className='text-lg font-semibold border-t-2 border-sky-300 rounded-lg'>{review}</h1>
+            </div>
         </div>
     );
 };
