3efb73a122eba59e8ea96fe9376e45dbcb54e068
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-10-10 11:53:12 +0600

    resume page Done

--- src/app/(profile)/profile/(JobSeeker)/viewResume/page.jsx
+++ src/app/(profile)/profile/(JobSeeker)/viewResume/page.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+"use client"
 
-const ViewResume = () => {
+import ResumePDF from "@/components/ResumePDF/ResumePDF";
+import { useRef } from "react";
+import { useReactToPrint } from 'react-to-print';
+import { AiFillPrinter } from 'react-icons/ai'
+
+const Resume = () => {
+    const contentRef = useRef(null);
+    const reactToPrintFn = useReactToPrint({ contentRef });
     return (
         <div>
-            ViewResume
+           
+            <div ref={contentRef}><ResumePDF/></div>
+            <button  className="mt-4 p-2 hover:bg-rose-700  bg-blue-500 text-white rounded flex mx-auto justify-center items-center gap-2 font-bold" onClick={reactToPrintFn}> <AiFillPrinter/>Print Resume</button>
         </div>
     );
 };
 
-export default ViewResume;
+export default Resume;
