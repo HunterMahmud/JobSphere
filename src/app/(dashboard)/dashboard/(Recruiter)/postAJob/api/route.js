acda8c3425d85e27e95fe7bfa89067c9344bf870
huntermahmud <mahmudthedatahunter@gmail.com>
2024-10-10 19:09:10 +0600

    fix: post a job api updated

--- src/app/(dashboard)/dashboard/(Recruiter)/postAJob/api/route.js
+++ src/app/(dashboard)/dashboard/(Recruiter)/postAJob/api/route.js
@@ -2,11 +2,14 @@ import { connectDB } from "@/lib/connectDB";
 
 export const POST = async (request) => {
   const newJobs = await request.json();
+  console.log(newJobs);
   const db = await connectDB();
   const postAJobCollection = db.collection("addedJobs");
+  const companyCollection = db.collection("company");
 
   try {
-    const res = await postAJobCollection.insertOne(newJobs);
+    const result = await companyCollection.findOne({'contactInformation.email':newJobs?.email})
+    const res = await postAJobCollection.insertOne({...newJobs, compnayInforamtion: result});
     return Response.json({ message: "job posted successfully",res }, { status: 200 });
   } catch (error) {
     return Response.json({ message: "Something Went Wrong" }, { status: 400 });
