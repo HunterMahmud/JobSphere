2a75e9aefeab4caffeaa7869827aa59a31a940d6
huntermahmud <mahmudthedatahunter@gmail.com>
2024-10-13 08:30:03 +0600

    fix: compaines page.jsx error

--- src/app/companies/page.jsx
+++ src/app/companies/page.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import  axios  from "axios";
 import { toast } from 'react-hot-toast';
 
-const page = () => {
+const Companies = () => {
   const [companies, setCompanies] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -80,4 +80,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Companies;
