fbf8e801e0ce35eb8cf3c90666443f5c79003709
rafizul896 <rafizulislam896@gmail.com>
2024-10-19 16:13:37 +0600

    update: jobCard design

--- src/components/Jobs/JobCard.jsx
+++ src/components/Jobs/JobCard.jsx
@@ -1,79 +1,79 @@
-import Link from "next/link";
-import React from "react";
+'use client'
+import { AiOutlineDollarCircle } from "react-icons/ai";
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaBookmark, FaClock, FaRegBookmark } from 'react-icons/fa';
+import { formatDistanceToNow } from 'date-fns';
+import { PiBagSimpleFill } from 'react-icons/pi';
+import { FaLocationDot } from 'react-icons/fa6';
+
+export const getPostedTimeAgo = (postedDate) => {
+    return formatDistanceToNow(new Date(postedDate), { addSuffix: true });
+};
 
 const JobCard = ({ job }) => {
-  const {
-    _id,
-    postedDate,
-    deadline,
-    skills,
-  } = job;
+    const [love, setLove] = useState(false)
 
-  return (
-    <div>
-      <div className="card mx-3 md:mx-0 bg-sky-50 shadow-2xl p-6 rounded-xl border-l-4 border-slate-800 flex flex-col justify-between hover:border-2 hover:border-sky-500 box-border h-min-[350px]">
-        <div className="card-body">
-          <h2 className="card-title text-xl font-semibold animate-pulse">
-            {job?.jobTitle}
-          </h2>
-          <p className="text-gray-700 font-medium">
-            {job?.compnayInforamtion?.companyInfo?.companyName}
-          </p>
-          <div className="text-gray-600 mt-2">
-            <p>
-              <span className="font-bold">Job Type:</span>{" "}
-              {job?.jobType}
-            </p>
-            <p>
-              <span className="font-bold">Experience Need:</span>{" "}
-             <span className="font-bold"> {job?.experience}</span>
-            </p>
-            <p>
-              <span className="font-bold">Posted Date:</span> {new Date(postedDate).toLocaleDateString('en-US', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-                hour12: true // For 12-hour format, set to false for 24-hour format
-              })}
-            </p>
-            <p>
-              <span className="font-bold">Deadline:</span>{new Date(deadline).toLocaleDateString('en-US', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-                hour12: true // For 12-hour format, set to false for 24-hour format
-              })}
-            </p>
-          </div>
-          <div className="mt-4">
-            <p className="font-bold">Skill need:</p>
-            <div className="space-x-2 mt-2 flex flex-wrap gap-1">
-              {skills?.map((skill, index) => (
-                <span
-                  key={index}
-                  className="badge badge-outline badge-primary bg-accent px-3 rounded-lg border-2 border-blue-400"
-                >
-                  {skill}
-                </span>
-              ))}
+    const { _id, jobTitle, jobType, postedDate, salaryScale, applicantsNumber, compnayInforamtion } = job
+    return (
+        <div className="border w-full max-w-sm px-3 py-4 bg-[url('https://i.ibb.co/hch8Kbm/ix-GTl1715763309.png')] rounded-md shadow-md hover:shadow-2xl hover:scale-[1.01] transition-all space-y-2 text-black">
+            <div className='flex justify-between'>
+
+                <div className='flex gap-3'>
+                    <Image className='border rounded-full' src={compnayInforamtion?.companyInfo?.logo} width={60} height={50} />
+                    <p className='flex flex-col gap-1'>
+                        <h1 className='text-xl font-semibold '>
+                            {jobTitle}
+                        </h1>
+                        <p className='text-start'>{compnayInforamtion?.companyInfo?.companyName}</p>
+                    </p>
+                </div>
+
+                <div onClick={() => setLove(!love)} className='text-[22px] cursor-pointer'>
+                    {
+                        love ?
+                            <FaBookmark /> :
+                            <FaRegBookmark />
+                    }
+                </div>
             </div>
-          </div>
-        </div>
-        <div className="flex justify-center mt-3">
-          <button className="bg-primary btn hover:bg-hover text-white font-semibold py-2 px-6 rounded-lg w-auto">
-            <Link href={`/jobs/${_id}`}>View Details</Link>
-          </button>
-        </div>
-      </div>
-    </div>
 
-  );
-};
+            <div className='flex items-center gap-1'>
+                <span><AiOutlineDollarCircle /></span>
+                {salaryScale}
+            </div>
+
+            <div className='flex justify-between'>
+                <div className="flex justify-betwee gap-2 items-center">
+                    <span className="font-medium text-gray-600"><PiBagSimpleFill /></span>
+                    <span className="text-gray-800">{jobType}</span>
+                </div>
+
+                <div className="flex justify-betwee gap-2 items-center">
+                    <span className="font-medium text-gray-600"><FaLocationDot /></span>
+                    <span className="text-gray-800">{job?.locationType}</span>
+                </div>
+            </div>
+
+            <div className="flex justify-between items-center">
+                <p >
+                    Applicants Number: {applicantsNumber}
+                </p>
+
+                <div className="flex items-center">
+                    {/* Clock Icon */}
+                    <FaClock className="text-gray-500 mr-2" />
+                    {/* Time ago text */}
+                    <span className="text-sm text-gray-800">{getPostedTimeAgo(postedDate)}</span>
+                </div>
+            </div>
+
+            <Link href={`/jobs/${_id}`}>
+                <button className="btn border-0 py-2 rounded-md w-full bg-primary hover:bg-hover text-white mt-4 rounded-ful">View Details</button>
+            </Link>
+        </div>
+    )
+}
 
 export default JobCard;
