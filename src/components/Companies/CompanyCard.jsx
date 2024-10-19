f6cfc4beb1f12e67889ca17a8ae3727426cb4179
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-10-19 13:05:22 +0600

    button change on compay card

--- src/components/Companies/CompanyCard.jsx
+++ src/components/Companies/CompanyCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const CompanyCard = ({ company }) => {
   return (
-    <div className="min-w-full mx-auto bg-white  ring ring-accent   shadow-lg rounded-xl overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+    <div className="min-w-full mx-auto bg-white  ring ring-accent shadow-lg rounded-lg overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-xl">
       <div className="flex items-center p-5 border-b">
         {/* Company Logo */}
         <div className="w-20 h-20 bg-gray-200 rounded-full flex justify-center items-center overflow-hidden relative">
@@ -20,7 +20,7 @@ const CompanyCard = ({ company }) => {
         <div className="ml-4">
           <h2 className="text-xl font-semibold text-gray-800">{company?.companyInfo?.companyName}</h2>
           <p className="text-gray-500 flex items-center">
-            <FaBuilding className="mr-1" />
+            <FaBuilding className="mr-1 text-secondary font-semibold" />
             {company?.companyInfo?.companyType}
           </p>
         </div>
@@ -28,16 +28,16 @@ const CompanyCard = ({ company }) => {
       {/* Company Details */}
       <div className="p-5">
         <div className="text-gray-700 flex items-center mb-3">
-          <FaMapMarkerAlt className="mr-2" />
+          <FaMapMarkerAlt className="mr-2 text-secondary font-semibold" />
           <span>{company?.companyInfo?.address}</span>
         </div>
         <div className="text-gray-700 flex items-center mb-5">
-          <FaBriefcase className="mr-2" />
-          <span>{company?.companyInfo?.typesOfJobs?.join(', ')}</span>
+          <FaBriefcase className="mr-2 text-secondary font-semibold" />
+          <span>{company?.employmentInfo?.typesOfJobs}</span>
         </div>
         {/* View Details Button */}
         <div className="text-right">
-          <button className="bg-primary text-white px-4 py-2 rounded-lg transition-colors duration-300 hover:bg-hover hover:shadow-md">
+          <button className="bg-primary bg-opacity-90 text-white px-4 py-2 rounded-lg transition-colors duration-300 hover:bg-hover hover:shadow-md">
             View Details
           </button>
         </div>
