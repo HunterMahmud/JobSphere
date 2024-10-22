86978c9e2c858cb575a583160082108028cbde02
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-10-22 10:50:17 +0600

    sort the jobs according to recent posted jobs

--- src/app/jobs/api/route.js
+++ src/app/jobs/api/route.js
@@ -7,9 +7,9 @@ export const GET = async (request) => {
 
   // Get search query and pagination parameters from the URL
   const { searchParams } = new URL(request.url);
-  const search = searchParams.get('search') || ''; // Get the search parameter
-  const page = parseInt(searchParams.get('page')) || 1; // Default to page 1 if not provided
-  const limit = parseInt(searchParams.get('limit')) || 9; // Default to 9 jobs per page
+  const search = searchParams.get("search") || ""; // Get the search parameter
+  const page = parseInt(searchParams.get("page")) || 1; // Default to page 1 if not provided
+  const limit = parseInt(searchParams.get("limit")) || 9; // Default to 9 jobs per page
   const skip = (page - 1) * limit; // Skip jobs for pagination
 
   try {
@@ -17,9 +17,10 @@ export const GET = async (request) => {
     const jobs = await jobsCollection
       .find({
         $or: [
-          { 'jobTitle': { $regex: search, $options: 'i' } }, // Case-insensitive search for job title
-        ]
+          { jobTitle: { $regex: search, $options: "i" } }, // Case-insensitive search for job title
+        ],
       })
+      .sort({ postedDate: -1 }) // Sort by createdAt field in descending order
       .skip(skip)
       .limit(limit)
       .toArray();
@@ -27,11 +28,16 @@ export const GET = async (request) => {
     // Get total number of jobs for pagination metadata
     const totalJobs = await jobsCollection.countDocuments({
       $or: [
-        { 'jobTitle': { $regex: search, $options: 'i' } }, // Case-insensitive search for job title
-      ]
+        { jobTitle: { $regex: search, $options: "i" } }, // Case-insensitive search for job title
+      ],
     });
 
-    return NextResponse.json({ jobs, totalJobs, currentPage: page, totalPages: Math.ceil(totalJobs / limit) });
+    return NextResponse.json({
+      jobs,
+      totalJobs,
+      currentPage: page,
+      totalPages: Math.ceil(totalJobs / limit),
+    });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: "No Data Found", error });
