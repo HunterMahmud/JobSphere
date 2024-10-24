d4e807dcb365df94bc7f8adc58560f7e8af72e28
huntermahmud <mahmudthedatahunter@gmail.com>
2024-10-24 11:51:19 +0600

    update: layout of recents jobs

--- src/components/Homepage/RecentJobs.jsx
+++ src/components/Homepage/RecentJobs.jsx
@@ -30,7 +30,7 @@ const RecentJobs = () => {
     return (
         <div>
             <h1 className="text-3xl font-extrabold text-center mb-8">Recent Jobs</h1>
-            <div className="grid container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-11/12 md:w-5/6 lg:w-4/5 mx-auto">
+            <div className="grid container grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 w-11/12 md:w-5/6 lg:w-4/5 mx-auto">
                 {
                     jobs?.length > 0 ? jobs.map(job => <JobCard key={job?._id} job={job} />) : <h3 className='text-center mt-5 md:grid-cols-2 lg:col-span-3'>No jobs Found</h3>
                 }
@@ -39,7 +39,7 @@ const RecentJobs = () => {
             <Link 
             className=' my-10 flex justify-center items-center'
             href={"/jobs"} >
-                <button className='bg-blue-600  hover:bg-purple-600  py-3 px-6 shadow-2xl text-white font-bold  rounded-md text-sm transition-colors duration-300 ease-in-out'> View All Jobs</button>
+                <button className='bg-primary  hover:bg-hover  py-3 px-6 shadow-2xl text-white font-bold  rounded-md text-sm transition-colors duration-300 ease-in-out'> View All Jobs</button>
 
             </Link>
 
