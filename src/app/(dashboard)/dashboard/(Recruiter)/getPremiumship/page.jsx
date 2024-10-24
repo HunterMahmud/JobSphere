cd18b85170c29083b40a7ab33f72e105f7dc8af9
tajtanvvir1 <tanvirah1223@gmail.com>
2024-10-24 15:47:56 +0600

    getPremiumship file added

---
+++ src/app/(dashboard)/dashboard/(Recruiter)/getPremiumship/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GetPremiumship = () => {
+    return (
+        <div>
+            <button className='m-10 px-2 py-4 rounded-xl'>Buy Now</button>
+        </div>
+    );
+};
+
+export default GetPremiumship;
