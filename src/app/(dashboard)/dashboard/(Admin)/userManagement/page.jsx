e468ffbb3d3f155c45bb898fe658ae3b1a0f7e72
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-10-26 12:30:10 +0600

    stutus coloum added on usermanagement page

--- src/app/(dashboard)/dashboard/(Admin)/userManagement/page.jsx
+++ src/app/(dashboard)/dashboard/(Admin)/userManagement/page.jsx
@@ -127,6 +127,7 @@ const UserManagement = () => {
                 <th className="px-6 py-4 text-left font-medium text-gray-700">Name</th>
                 <th className="px-6 py-4 text-left font-medium text-gray-700">Role</th>
                 <th className="px-6 py-4 text-left font-medium text-gray-700">Email</th>
+                <th className="px-6 py-4 text-left font-medium text-gray-700">Status</th>
                 <th className="px-6 py-4 text-left font-medium text-gray-700">Action</th>
               </tr>
             </thead>
@@ -137,22 +138,37 @@ const UserManagement = () => {
                   <td className="py-4 px-6 text-gray-800">
                     {user.name || user.fullName || user.userName}
                   </td>
-                  <td className="px-6 py-4">
+                  <td className="px-6 py-2">
                     <span className="inline-block px-2 py-1 font-medium rounded-full bg-blue-100 text-blue-600">
                       {user.role}
                     </span>
                   </td>
-                  <td className="py-4 px-6 text-gray-800">{user.email}</td>
+                  <td className="py-4 px-6 text-gray-800 sm:text-xs">{user.email}</td>
+                  <td className="py-4 px-4 text-gray-800 ">
+                    <div className="flex items-center gap-1">
+                    <span class={`h-1.5 w-1.5 rounded-full ${user?.status ==="blocked"? "text-red-500 " : "bg-secondary"}`}></span>
+                    <span
+                    className={`${user?.status === "blocked"
+                      ? "text-red-500 "  
+                      : "text-primary" 
+                      } py-1 bg-opacity-60 rounded-full font-semibold`}
+                  >
+                    {user?.status === "block" ? "Blocked" : "active"}
+                  </span>
+                    </div>
+                  </td>
                   <td className="py-4 px-6">
                     <button
-                      className={`bg-red-500 text-white py-1 px-3 rounded-md transition ${
-                        user.role === "admin" ? "cursor-not-allowed opacity-50" : "hover:bg-red-600"
-                      }`}
+                      className={`bg-red-500 text-white py-1 px-3 rounded-md transition ${user.role === "admin" ? "cursor-not-allowed opacity-50" : "hover:bg-red-600"
+                        }`}
                       onClick={() => handleDelete(user.email, user.role)}
                       disabled={user.role === "admin"}
                     >
                       <AiFillDelete />
                     </button>
+                    {
+                      <Button variant="destructive">Destructive</Button>
+                    }
                   </td>
                 </tr>
               ))}
@@ -161,78 +177,76 @@ const UserManagement = () => {
         )}
       </div>
 
-     {/* Pagination  */}
-     <div className="flex items-center justify-between bg-gray-50 px-6 py-4 border-t">
-            <div className="flex items-center space-x-2">
-              <span className="text-gray-700">View</span>
-              <select
-                value={limit}
-                onChange={(e) => {
-                  setLimit(parseInt(e.target.value)), setPage(1);
-                }}
-                className="border border-gray-300 rounded-md py-1 px-3"
-              >
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="30">30</option>
-              </select>
-              <span className="text-gray-700 block w-full pr-6">
-                Applicants per page
-              </span>
-            </div>
-            <div className="flex items-center space-x-2">
-              <button
-                disabled={page === 1}
-                onClick={() => setPage(page - 1)}
-                className={`${
-                  page === 1 ? "cursor-not-allowed text-gray-400": "text-gray-700"
-                }`}
-              >
-                <span className="sr-only">Prev Page</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-6"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-              <div className="space-x-2 flex">
-                <p className="text-base text-gray-900">
-                  {page}
-                  <span className="mx-0.25">/</span>
-                  {Math.ceil(totalUsers / limit)}
-                </p>
-              </div>
-
-              <button
-                disabled={page === Math.ceil(totalUsers / limit)}
-                onClick={() => setPage(page + 1)}
-                className={`${
-                  page === Math.ceil(totalUsers / limit) ? "cursor-not-allowed text-gray-400": "text-gray-700 "
-                }`}
-              >
-                <span className="sr-only">Next Page</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-6"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
+      {/* Pagination  */}
+      <div className="flex items-center justify-between bg-gray-50 px-6 py-4 border-t">
+        <div className="flex items-center space-x-2">
+          <span className="text-gray-700">View</span>
+          <select
+            value={limit}
+            onChange={(e) => {
+              setLimit(parseInt(e.target.value)), setPage(1);
+            }}
+            className="border border-gray-300 rounded-md py-1 px-3"
+          >
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+          </select>
+          <span className="text-gray-700 block w-full pr-6">
+            Applicants per page
+          </span>
+        </div>
+        <div className="flex items-center space-x-2">
+          <button
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
+            className={`${page === 1 ? "cursor-not-allowed text-gray-400" : "text-gray-700"
+              }`}
+          >
+            <span className="sr-only">Prev Page</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+          <div className="space-x-2 flex">
+            <p className="text-base text-gray-900">
+              {page}
+              <span className="mx-0.25">/</span>
+              {Math.ceil(totalUsers / limit)}
+            </p>
           </div>
+
+          <button
+            disabled={page === Math.ceil(totalUsers / limit)}
+            onClick={() => setPage(page + 1)}
+            className={`${page === Math.ceil(totalUsers / limit) ? "cursor-not-allowed text-gray-400" : "text-gray-700 "
+              }`}
+          >
+            <span className="sr-only">Next Page</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
