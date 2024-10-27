9892504e25c103bf5962e8799f91272d715b73ab
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-10-27 10:34:05 +0600

    overview cards added

--- src/app/(dashboard)/dashboard/(Admin)/statistics/page.jsx
+++ src/app/(dashboard)/dashboard/(Admin)/statistics/page.jsx
@@ -1,17 +1,13 @@
 import JobPostingsChart from '@/components/Chart/JobPostingsChart';
 import AppliedJobsChart from '@/components/Chart/AppliedJobsChart';
+import OverviewCard from '@/components/OverviewCard/OverviewCard';
 
 const Statistics = () => {
     return (
         <div className="container mx-auto px-4 py-8">
-            {/* <div className="md:flex gap-6">
-                <div className="md:w-1/2 h-[400px] flex items-center justify-center">
-                    <JobPostingsChart className="w-full h-full" />
-                </div>
-                <div className="md:w-1/2 h-[400px] flex items-center justify-center">
-                    <AppliedJobsChart className="w-full h-full" />
-                </div>
-            </div> */}
+            <div>
+                <OverviewCard/>
+            </div>
             <div className="lg:flex gap-6">
             <JobPostingsChart/>
             <AppliedJobsChart/>
