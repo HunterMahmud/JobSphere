08ade8ca7119f8b1f0047bd0c87bc1d41a92848f
tajtanvvir1 <tanvirah1223@gmail.com>
2024-10-28 15:56:28 +0600

    Payment Working

--- src/app/(dashboard)/dashboard/(Recruiter)/getPremium/page.jsx
+++ src/app/(dashboard)/dashboard/(Recruiter)/getPremium/page.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
-import { FcOk } from "react-icons/fc";
+"use client";
 
-const getPremium = () => {
+import Modal from '@/components/Modal/Modal';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { FcOk } from "react-icons/fc";
+import toast from "react-hot-toast";
+import useRole from '@/components/Hooks/useRole';
+
+
+const GetPremium = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [method, setMethod] = useState([{
+        "name": "VISA",
+        "type": "visa",
+        "logo": "https://sandbox.sslcommerz.com/gwprocess/v4/image/gw/visa.png",
+        "gw": "visacard",
+        "r_flag": "1",
+        "redirectGatewayURL": "https://sandbox.sslcommerz.com/gwprocess/v4/bankgw/indexhtmlOTP.php?mamount=1000.00&ssl_id=2310191520231MLVg8ZTsa9Ld4k&Q=REDIRECT&SESSIONKEY=9CE83C4562A96645C7652AF10D220C37&tran_type=success&cardname=visavard"
+    },]);
+
+    const { loggedInUser } = useRole();
+
+    const PaymentOption = async () => {
+        if (loggedInUser?.role === "seeker") {
+            return toast.error("Action not permitted!");
+        } else {
+            setIsLoading(true);
+            setShowModal(!showModal);
+            let res = await fetch("/api/payment", { method: 'POST' });
+            let JSON = await res.json();
+            setIsLoading(false);
+            setMethod(JSON['data']['desc'])
+        }
+    };
+
+    const PayNow = (PayURL) => {
+        window.location.replace(PayURL);
+    }
+
+
+
     return (
         <div>
             <h1 className="text-2xl font-bold text-center mb-8">Get Premium</h1>
@@ -19,7 +58,7 @@ const getPremium = () => {
                     </div>
                     <div>
                         <button
-                            // onClick={handleApplyNow}
+                            onClick={PaymentOption}
                             className="bg-primary flex justify-center mx-auto text-sm md:text-base text-white px-4 md:px-6 py-2 rounded-lg hover:bg-hover transition"
                         >
                             Buy 1 Month Premium
@@ -38,15 +77,37 @@ const getPremium = () => {
                         <p className='text-lg flex items-center gap-2'><span><FcOk /></span>Priority Listing for Companies for 1 Year</p>
                     </div>
                     <button
-                        // onClick={handleApplyNow}
+                        onClick={PaymentOption}
                         className="bg-primary flex justify-center mx-auto text-sm md:text-base text-white px-4 md:px-6 py-2 rounded-lg hover:bg-hover transition"
                     >
                         Buy 1 Year Premium
                     </button>
                 </div>
             </div>
+
+            {/* Modal */}
+            {
+                <Modal isVisible={showModal} showModal={showModal} setShowModal={setShowModal}>
+                    <div className='grid grid-cols-3 h-3/4'>
+                        {
+                            method.map((item, i) => {
+                                return (
+                                    <div key={i} className="col-md-2 col-lg-2 col-6 p-1">
+                                        <div className="card  h-100 bg-white shadow-sm">
+                                            <a onClick={() => { PayNow(item['redirectGatewayURL']) }}>
+                                                <Image alt='logo' className="w-100 pay-img" width={100} height={100} src={item['logo']} />
+                                            </a>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </Modal>
+            }
+
         </div>
     );
 };
 
-export default getPremium;
+export default GetPremium;
