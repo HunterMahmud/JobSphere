aef236866b66c9f1c1cc612b7ae2b61f2587edfa
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-10-30 14:50:54 +0600

    applyedjob table updated

--- src/app/(dashboard)/dashboard/(seeker)/applyedJobs/page.jsx
+++ src/app/(dashboard)/dashboard/(seeker)/applyedJobs/page.jsx
@@ -194,9 +194,13 @@ const ApplyedJobs = () => {
         </div>
 
         {/* Table */}
-        <div className="overflow-x-auto border rounded-lg shadow-md">
+        <div className="overflow-x-auto border rounded-lg shadow-md min-h-96">
           {
-            loading ? <Loader /> : <table className="min-w-full bg-white">
+            loading ? <Loader /> : <>
+               {
+                jobs.length>0?<>
+                
+                <table className="min-w-full bg-white">
               {/* Table Header */}
               <thead className="bg-gray-50 border-b">
                 <tr>
@@ -213,7 +217,7 @@ const ApplyedJobs = () => {
               <tbody>
                 {jobs?.map((job, index) => (
                   <tr key={index} className="border-b hover:bg-gray-50 text-xs md:text-sm">
-                    <td className="px-6 py-4">{index + 1}</td>
+                    <td className="px-6 py-4">{(page - 1) * limit + index + 1}</td>
                     <td className="px-1 md:px-3 lg:px-6 py-4 flex items-center gap-2 hover:underline">
                       <Link href={`/jobs/${job?.jobId}`}>
                         {job?.jobTitle}
@@ -409,78 +413,84 @@ const ApplyedJobs = () => {
                 ))}
               </tbody>
             </table>
+              {/* Pagination  */}
+              <div className="flex items-center justify-between bg-gray-50 md:px-6 py-4 border-t">
+                <div className="flex items-center space-x-2">
+                  <span className="text-gray-700">View</span>
+                  <select
+                    value={limit}
+                    onChange={(e) => {
+                      setLimit(parseInt(e.target.value)), setPage(1);
+                    }}
+                    className="border border-gray-300 rounded-md py-1 px-3"
+                  >
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="30">30</option>
+                  </select>
+                  <span className="text-gray-700 block w-full pr-6">
+                    Applicants per page
+                  </span>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <button
+                    disabled={page === 1}
+                    onClick={() => setPage(page - 1)}
+                    className={`${page === 1 ? "cursor-not-allowed text-gray-400" : "text-gray-700"
+                      }`}
+                  >
+                    <span className="sr-only">Prev Page</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="size-6"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  <div className="space-x-2 flex">
+                    <p className="text-base text-gray-900">
+                      {page}
+                      <span className="mx-0.25">/</span>
+                      {Math.ceil(total / limit)}
+                    </p>
+                  </div>
+
+                  <button
+                    disabled={page === Math.ceil(total / limit)}
+                    onClick={() => setPage(page + 1)}
+                    className={`${page === Math.ceil(total / limit) ? "cursor-not-allowed text-gray-400" : "text-gray-700 "
+                      }`}
+                  >
+                    <span className="sr-only">Next Page</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="size-6"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+                </>:<h1 className="text-center font-semibold mt-10">No job found</h1>
+
+               }
+          
+            </>
           }
 
-          {/* Pagination  */}
-          <div className="flex items-center justify-between bg-gray-50 md:px-6 py-4 border-t">
-            <div className="flex items-center space-x-2">
-              <span className="text-gray-700">View</span>
-              <select
-                value={limit}
-                onChange={(e) => {
-                  setLimit(parseInt(e.target.value)), setPage(1);
-                }}
-                className="border border-gray-300 rounded-md py-1 px-3"
-              >
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="30">30</option>
-              </select>
-              <span className="text-gray-700 block w-full pr-6">
-                Applicants per page
-              </span>
-            </div>
-            <div className="flex items-center space-x-2">
-              <button
-                disabled={page === 1}
-                onClick={() => setPage(page - 1)}
-                className={`${page === 1 ? "cursor-not-allowed text-gray-400" : "text-gray-700"
-                  }`}
-              >
-                <span className="sr-only">Prev Page</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-6"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-              <div className="space-x-2 flex">
-                <p className="text-base text-gray-900">
-                  {page}
-                  <span className="mx-0.25">/</span>
-                  {Math.ceil(total / limit)}
-                </p>
-              </div>
 
-              <button
-                disabled={page === Math.ceil(total / limit)}
-                onClick={() => setPage(page + 1)}
-                className={`${page === Math.ceil(total / limit) ? "cursor-not-allowed text-gray-400" : "text-gray-700 "
-                  }`}
-              >
-                <span className="sr-only">Next Page</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-6"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
         </div>
       </div>
     </Fragment >
