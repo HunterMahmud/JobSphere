b8c17c1b420fbeb68165d57e348da1d360307ce6
huntermahmud <mahmudthedatahunter@gmail.com>
2024-10-30 14:53:35 +0600

    calculate: all the status number calculated

--- src/app/api/user-progress/route.js
+++ src/app/api/user-progress/route.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   const db = await connectDB();
+  
   const { searchParams } = new URL(request.url);
   const email = searchParams.get("email");
 
@@ -13,14 +14,16 @@ export const GET = async (request) => {
       db.collection("seekerInfo").findOne({ "contactInformation.email": email }),
       db.collection("jobs").find({}).toArray()
     ]);
-
+    console.log("applied jobs col: ",applyedJobsCollection)
     // Logic for calculating profile completion (assuming fields weights)
     const profileFields = ['contactInformation', 'profileOverview', 'careerObjective', 'projects', 'skills', 'education', 'workExperience'];
     const completedFields = profileFields.filter(field => seekerInfoCollection && seekerInfoCollection[field]);
     const profileCompletion = Math.floor((completedFields.length / profileFields.length) * 100);
 
+    const acc = { "Pending": 0, "Task": 0, "Interview": 0, "Submitted": 0, "Selected": 0, "Rejected": 0 };
+    
     // Count applications per status
-    const jobStatusCounts = applyedJobsCollection.reduce((acc, job) => {
+    const jobStatusCounts = applyedJobsCollection.reduce((_, job) => {
       acc[job.jobStatus] = (acc[job.jobStatus] || 0) + 1;
       return acc;
     }, {});
@@ -28,7 +31,7 @@ export const GET = async (request) => {
     // Calculate growth logic for different states
     const calculateProgress = () => {
       let progress = 0;
-      const weights = { "Pending": 0.1, "Tasked": 0.3, "Interviewed": 0.4, "Selected": 0.8, "Rejected": -0.2 };
+      const weights = { "Pending": 0.1, "Task": 0.3, "Interview": 0.4, "Submitted": 0.5, "Selected": 0.8, "Rejected": -0.2 };
       applyedJobsCollection.forEach(job => {
         const weight = weights[job.jobStatus] || 0;
         progress += weight;
